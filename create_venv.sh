15c93397cdb26db90db9ad93fef60a3234244462
Yi Li <yil8@uci.edu>
2017-08-20 10:19:54 -0700

    Update create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -1,5 +1,5 @@
 python3 -m venv py3.5
 source py3.5/bin/activate
 
-pip install 'numpy==1.12.0' -i https://pypi.python.org/simple/
+pip install 'numpy==1.12.1' -i https://pypi.python.org/simple/
 pip install -r requirements.txt -i https://pypi.python.org/simple/
