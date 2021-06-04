988fec92f15e1ed9dd9d258b41627349ff35ca17
Yi Li <yil8@uci.edu>
2021-06-04 08:45:24 +0800

    Update create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -4,4 +4,4 @@ source py3.5/bin/activate
 pip install 'numpy==1.12.1' -i https://pypi.python.org/simple/
 pip install -r requirements.txt -i https://pypi.python.org/simple/
 
-# mirror in China : https://pypi.tuna.tsinghua.edu.cn/simple
+# mirror in China : https://mirrors.aliyun.com/pypi/simple/
